e01328bca8eddf0e3499df17259a0ed35d09b36a
SongYun <william0228.cs05@g2.nctu.edu.tw>
2019-01-16 04:02:09 +0800

    add

--- login_server.py
+++ login_server.py
@@ -143,6 +143,7 @@ class DBControl(object):
                 server_ip = ""
 
                 res3 = Server_connect.select(Server_connect.server_ip, Server_connect.instance_id).group_by(Server_connect.server_ip).having(fn.Count(Server_connect.user) < 10)
+                print (res3)
                 if (len(res3) == 0):
                     server_ip, instance_id = Create_instance()
                 else:
